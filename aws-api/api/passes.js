484188df146e825ffe7eee92a1b9f32bbc2eb856
alonsovargas3 <alonsovargas@gmail.com>
2020-03-08 15:10:47 -0500

    Created Serverless api in the aws-api folder that will get all the passes in the DynamoDB table

---
+++ aws-api/api/passes.js
@@ -0,0 +1,33 @@
+'use strict';
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+
+module.exports.list = (event, context, callback) => {
+    var params = {
+        TableName: process.env.PASS_TABLE,
+        ProjectionExpression: "passDate,passTime"
+    };
+
+    console.log("Scanning passes table.");
+    const onScan = (err, data) => {
+
+        if (err) {
+            console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+            callback(err);
+        } else {
+            console.log("Scan succeeded.");
+            return callback(null, {
+                statusCode: 200,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    passes: data.Items
+                })
+            });
+        }
+
+    };
+
+    dynamoDb.scan(params, onScan);
+
+};
